16651dc48c948de40637e944f56e4a209f7ca932
sugardraw <sergiousl@yahoo.com>
2019-03-01 19:06:07 +0100

    changes in package Json

---
+++ build/precache-manifest.70b8e4e2d78b1d54defc02ca1587dd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f8820f77d765c62ea94038268550484",
+    "url": "/sugardraw/portfolio_website/static/media/dummy.7f8820f7.obj"
+  },
+  {
+    "revision": "d5d7b175aaade2df0421",
+    "url": "/sugardraw/portfolio_website/static/js/runtime~main.d5d7b175.js"
+  },
+  {
+    "revision": "86db3fdcef8b2d444d04",
+    "url": "/sugardraw/portfolio_website/static/js/main.86db3fdc.chunk.js"
+  },
+  {
+    "revision": "818072a37e48ea0466c7",
+    "url": "/sugardraw/portfolio_website/static/js/1.818072a3.chunk.js"
+  },
+  {
+    "revision": "86db3fdcef8b2d444d04",
+    "url": "/sugardraw/portfolio_website/static/css/main.7d804346.chunk.css"
+  },
+  {
+    "revision": "090d9b1551998a0cc0edb507410f2d4b",
+    "url": "/sugardraw/portfolio_website/index.html"
+  }
+];
